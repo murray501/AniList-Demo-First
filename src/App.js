5a86be26ae69bd070bf1fc032adfb33750ba6ed0
murray501 <m68286832@gmail.com>
2021-12-08 22:42:03 +0900

    request GraphQL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { GraphQLClient } from "graphql-request";
+
+const query = `
+{
+  Page {
+    media {
+      siteUrl
+      title {
+        english
+        native
+      }
+      description
+    }
+  }
+}
+`;
+
+const client = new GraphQLClient(
+  "https://graphql.anilist.co"
+);
 
 function App() {
+  const [Data, setData] = useState();
+
+  useEffect(() => {
+    client
+      .request(query)
+      .then(setData)
+      .catch(console.error);
+
+  }, [client, query])
+
+  if (!Data) return <p>loading...</p>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <List data={Data.Page.media} renderItem={Details} renderEmpty={<p>Nothing to render.</p>} /> 
+    </>
   );
 }
+
+function Details(data) {
+  return (
+    <section>
+      <p>{data.siteUrl}</p>
+      <p>{data.title.english}</p>
+      <p>{data.title.native}</p>
+      <p>{data.description}</p>  
+    </section>
+  );
+}
+
+function List({ data, renderItem, renderEmpty}) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+  <ul>
+    {
+      data.map((item, i) => (
+        <li key={i}>{Details(item)}</li>
+      ))
+    }
+  </ul>
+  );
+}
+
 
 export default App;
