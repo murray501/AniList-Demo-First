36da9f315c203e2d53e5b7d91718cc21c7901d57
murray501 <m68286832@gmail.com>
2021-12-10 21:27:54 +0900

    use react-list

--- src/pages/list.js
+++ src/pages/list.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactList from "react-list";
 
 const loadJSON = key => key && JSON.parse(localStorage.getItem(key));
 const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
@@ -35,9 +36,14 @@ function request() {
 export default function PageList() {
   const [data, setData] = useState(loadJSON(query));
 
+  const renderItem = (index, key) => {
+    return <div key={key}>{index+1} {data[index].title.english} - {data[index].title.native}</div>;
+  }
+
   useEffect(() => {
     if (!data) return;
     saveJSON(query, data);
+    console.log('data length=' + data.length);
   }, [data]);
 
   useEffect(() => {
@@ -50,35 +56,17 @@ export default function PageList() {
   }, [])
 
   if (!data) return <p>loading...</p>
+  if (!data.length) return <p>Nothing to render.</p>
 
   return (
     <>
-      <List data={data} renderItem={Details} renderEmpty={<p>Nothing to render.</p>} /> 
+    <div style={{overflow: 'auto', maxHeight: 400}}>
+      <ReactList
+        itemRenderer={renderItem}
+        length={data.length}
+        type='uniform'
+      />
+    </div> 
     </>
-  );
-}
-
-function Details(data) {
-  return (
-    <section>
-      <p>{data.siteUrl}</p>
-      <p>{data.title.english}</p>
-      <p>{data.title.native}</p>
-      <p>{data.description}</p>  
-    </section>
-  );
-}
-
-function List({ data, renderItem, renderEmpty}) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-  <ul>
-    {
-      data.map((item, i) => (
-        <li key={i}>{Details(item)}</li>
-      ))
-    }
-  </ul>
   );
 }
